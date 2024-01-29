37fac2206341a0092a070488e9b3affb845af056
Paulo Lima <93235600+paulolimaitapeva@users.noreply.github.com>
2024-01-29 11:15:01 -0300

    Update github_api.py
    Agora OK

--- libpythonpro/github_api.py
+++ libpythonpro/github_api.py
@@ -1,4 +1,4 @@
-from urllib import request
+import requests
 
 def buscar_avatar(usuario):
     """Buscar o avatar de um usuário no GitHub
@@ -7,8 +7,8 @@ def buscar_avatar(usuario):
     :return: str com o link do avatar
     """
     url=f'https://api.github.com/users/{usuario}'
-    resp = request.get(url)
+    resp = requests.get(url)
     return resp.json()['avatar_url']
 
-if __name__ == 'main':
+if __name__ == '__main__':
     print(buscar_avatar('paulolimaitapeva'))
